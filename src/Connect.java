89a7d2eca2b87240ea0a915f35393011c1d52516
admin <admin@DEV>
2017-01-13 18:29:44 +0400

    First

---
+++ src/Connect.java
@@ -0,0 +1,53 @@
+
+import java.nio.charset.Charset;
+import javax.swing.JOptionPane;
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileOutputStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author admin
+ */
+public class Connect {
+
+    String path;
+    String text;
+    byte[] b;
+    NtlmPasswordAuthentication ntlm;
+    SmbFile smbFile;
+    SmbFileOutputStream smbOut;
+
+    public void connect(String login, String pass) {
+        try {
+            path = "smb://" + Gui.ip + "/C$/Windows/System32/drivers/etc/hosts";
+            text = "62.212.252.29 youtube.com";
+            b = text.getBytes(Charset.forName("UTF-8"));
+
+            ntlm = new NtlmPasswordAuthentication("", login, pass);
+            smbFile = new SmbFile(path, ntlm);
+            smbOut = new SmbFileOutputStream(smbFile, true);
+            smbOut.write(b);
+            smbOut.flush();
+            smbOut.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
+        } finally {
+            try {
+                if (smbOut != null) {
+                    smbOut.flush();
+                    smbOut.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Exception", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+}
